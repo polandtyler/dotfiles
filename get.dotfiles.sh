caff09116b3bc762258b736889818870ac31c245
Avery Finn <wasbazi@gmail.com>
2024-07-11 08:48:40 -0600

    include colors in install script for ease

--- get.dotfiles.sh
+++ get.dotfiles.sh
@@ -13,6 +13,23 @@ REPO_NAME="dotfiles"
 REPO_DIR="$HOME/$REPO_NAME"
 GIT_REPO_URL="https://$REPO_HOST/$REPO_OWNER/$REPO_NAME.git"
 
+gray() {
+  yellow $1
+  # echo "$(tput bold)$(tput setaf 0)[*] $1$(tput sgr0)"
+}
+
+green() {
+  echo "$(tput setaf 2)[+] $1$(tput sgr0)"
+}
+
+yellow() {
+  echo "$(tput setaf 3)[!] $1$(tput sgr0)"
+}
+
+red() {
+  echo "$(tput setaf 1)[-] $1$(tput sgr0)"
+}
+
 set -e
 
 if [ ! -d ~/dotfiles ]; then
